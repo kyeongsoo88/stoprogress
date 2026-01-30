306a0000241920ffa53845a4ba1eb742b891fdf6
kyeongsoop88 <kyeongsoop@gmail.com>
2026-01-30 14:07:53 +0900

    Fix TypeScript type error for Plot component

--- src/components/PlotlyChart.tsx
+++ src/components/PlotlyChart.tsx
@@ -1,13 +1,15 @@
 "use client";
 
+import React from "react";
 import dynamic from "next/dynamic";
+import type { PlotParams } from "react-plotly.js";
 
 const Plot = dynamic(async () => {
   const PlotlyModule = await import("plotly.js-basic-dist");
   const createPlotlyComponent = (await import("react-plotly.js/factory")).default;
   const Plotly = "default" in PlotlyModule ? PlotlyModule.default : PlotlyModule;
   return createPlotlyComponent(Plotly);
-}, { ssr: false });
+}, { ssr: false }) as React.ComponentType<PlotParams>;
 
 export default Plot;
 
