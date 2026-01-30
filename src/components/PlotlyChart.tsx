672995dc0e47b233c960d29122c7a2d36c4df568
kyeongsoop88 <kyeongsoop@gmail.com>
2026-01-30 14:29:33 +0900

    Fix TypeScript errors by adding @ts-ignore for plotly modules

--- src/components/PlotlyChart.tsx
+++ src/components/PlotlyChart.tsx
@@ -5,7 +5,9 @@ import dynamic from "next/dynamic";
 import type { PlotParams } from "react-plotly.js";
 
 const Plot = dynamic(async () => {
+  // @ts-ignore - plotly.js-basic-dist doesn't have type definitions
   const PlotlyModule = await import("plotly.js-basic-dist");
+  // @ts-ignore - react-plotly.js/factory doesn't have type definitions
   const createPlotlyComponent = (await import("react-plotly.js/factory")).default;
   const Plotly = "default" in PlotlyModule ? PlotlyModule.default : PlotlyModule;
   return createPlotlyComponent(Plotly);
---
+++ src/types/plotly.d.ts
@@ -0,0 +1,6 @@
+declare module "plotly.js-basic-dist" {
+  const Plotly: any;
+  export default Plotly;
+  export = Plotly;
+}
+
