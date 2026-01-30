ad5f07903cad67651d49479e15c08c0c10ce4c76
kyeongsoop88 <kyeongsoop@gmail.com>
2026-01-30 14:11:16 +0900

    Add type declarations for react-plotly.js

---
+++ src/types/react-plotly.d.ts
@@ -0,0 +1,29 @@
+declare module "react-plotly.js" {
+  import { Component } from "react";
+  import { Data, Layout, Config } from "plotly.js";
+
+  export interface PlotParams {
+    data: Data[];
+    layout?: Partial<Layout>;
+    config?: Partial<Config>;
+    style?: React.CSSProperties;
+    className?: string;
+    onInitialized?: (figure: any, graphDiv: HTMLElement) => void;
+    onUpdate?: (figure: any, graphDiv: HTMLElement) => void;
+    onPurge?: (figure: any, graphDiv: HTMLElement) => void;
+    onError?: (err: Error) => void;
+    revision?: number;
+    useResizeHandler?: boolean;
+    debug?: boolean;
+  }
+
+  export default class Plot extends Component<PlotParams> {}
+}
+
+declare module "react-plotly.js/factory" {
+  import { Component } from "react";
+  import { PlotParams } from "react-plotly.js";
+
+  export default function createPlotlyComponent(plotly: any): React.ComponentType<PlotParams>;
+}
+
